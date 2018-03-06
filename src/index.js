a8becd4025589935a867e3ad1df7722bd1ce5014
atr <atr@ciklum.com>
2018-03-06 14:57:26 +0300

    Advanced zeros

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ module.exports = function getZerosCount(number, base) {
 
     lengths = [];
     var canonical = getCanonicalNumbers(base);
+    var exponent = canonical.reduce(function(acc, el) {
+        acc[el] = (acc[el] || 0) + 1;
+        return acc;
+    }, {});
+
+    canonical = unique (canonical);
 
     for (var key in canonical) {
-        lengths.push(getZeros(number, canonical[key]));
+        lengths.push(getZeros(number, canonical[key], exponent[canonical[key]]));
     }
 
     return Math.min.apply(null, lengths);
 
     function getCanonicalNumbers(base){
+        console.log(base)
         var j = 1;
         var i = 2;
         var canonical = [];
-        do {
+        while (i < base) {
             if (base % i == 0) {
                 canonical.push(i);
                 j++;
@@ -21,14 +28,14 @@ module.exports = function getZerosCount(number, base) {
             } else {
                 i++;
             }
-        } while (i < base);
+        }
 
         canonical.push(i);
 
-        return unique (canonical);
+        return canonical;
     }
 
-    function getZeros(numberModified, i){
+    function getZeros(numberModified, i, exponent){
         var count = 0;
         var j = i;
         while (j<=numberModified){
@@ -36,7 +43,7 @@ module.exports = function getZerosCount(number, base) {
             j = j*i
         }
 
-        return count;
+        return Math.floor(count/exponent);
     }
 
     function unique(arr) {
